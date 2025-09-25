71456c166932df6cad62aa429d94d01ad7468637
Kooky-ie <bad62573@gmail.com>
2025-09-25 15:38:34 +0300

    Added new files(README and main calculator backend )

---
+++ calculator.py
@@ -0,0 +1,27 @@
+from decimal import Decimal
+
+
+def main():
+    example = input()
+    elements = example.split(" ")
+    fst_num = Decimal(elements[0])
+    sec_num = Decimal(elements[2])
+    action = elements[1]
+    if action == "+":
+        print(fst_num + sec_num)
+    elif action == "-":
+        print(fst_num - sec_num)
+    elif action == "/":
+        if sec_num != 0:
+            print(fst_num / sec_num)
+        else:
+            print("Ошибка. Делить на ноль нельзя.")
+    elif action == "*":
+        print(fst_num * sec_num)
+
+
+try:
+    while __name__ == '__main__':
+        main()
+except KeyboardInterrupt:
+    print("Завершение программы")
